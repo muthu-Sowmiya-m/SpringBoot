24e8db7b319735b279e6d647445ecb88553f3036
muthu-Sowmiya-m <83859464+muthu-Sowmiya-m@users.noreply.github.com>
2022-04-14 14:13:31 +0530

    Delete main/java/com/Springboot directory

--- main/java/com/Springboot/DataTier/Service/TopicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SpringBoot.DataTier.Service;
-
-import com.SpringBoot.DataTier.Controller.Topics;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topics,String>
-{
-
-}
